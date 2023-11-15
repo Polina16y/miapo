b4ff922fcfd4ff56e582d3dcaba176e1b6bf6629
Денис Егоров <Sergeymatveev20001@gmail.com>
2023-11-16 00:06:14 +0300

    Добавьте файлы проекта.

---
+++ miapo.git/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double x, a, b;
+        Console.Write("Введите значение a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Введите значение b: ");
+        b = double.Parse(Console.ReadLine());
+        if (a > b)
+            x = a * b - 3;
+        else if (a == b)
+            x = 2;
+        else
+            x = (Math.Pow(a, 3) + 1) / b;
+        Console.WriteLine("Результат: " + x);
+        Console.ReadKey();
+    }
+}
